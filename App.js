315c1c86e7958e6e2bc09beb462db1ec10239927
zahidalidev <engrzahid612@gmail.com>
2020-11-06 22:58:22 +0500

    navigation with NavigationContainer

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createSwitchNavigator, createAppContainer } from "react-navigation";
+import { StyleSheet, Text, View, LogBox } from 'react-native';
+// import { createSwitchNavigator, createAppContainer } from "react-navigation";
 import firebase from "firebase"
+import { createDrawerNavigator } from "@react-navigation/drawer"
+import { NavigationContainer } from "@react-navigation/native"
 
 import LoadingScreen from "./screens/LoadingScreen"
 import LoginScreen from "./screens/LoginScreen"
@@ -12,19 +14,29 @@ import { firebaseConfig } from "./config"
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig)
 }
+LogBox.ignoreLogs(['Setting a timer for a long period of time'])
 
-const AppSwitchNavigator = createSwitchNavigator({
-  LoadingScreen: LoadingScreen,
-  LoginScreen: LoginScreen,
-  DashboardScreen: DashboardScreen
-})
+const Stack = createDrawerNavigator();
 
-const AppNavigator = createAppContainer(AppSwitchNavigator)
+
+// const AppSwitchNavigator = createSwitchNavigator({
+//   LoadingScreen: LoadingScreen,
+//   LoginScreen: LoginScreen,
+//   DashboardScreen: DashboardScreen
+// })
+
+// const AppNavigator = createAppContainer(AppSwitchNavigator)
 
 export default class App extends Component {
   render() {
     return (
-      <AppNavigator />
+      <NavigationContainer >
+        <Stack.Navigator initialRouteName="LoadingScreen">
+          <Stack.Screen name="LoadingScreen" >{(props) => <LoadingScreen {...props} />}</Stack.Screen>
+          <Stack.Screen name="LoginScreen" >{(props) => <LoginScreen {...props} />}</Stack.Screen>
+          <Stack.Screen name="DashboardScreen">{(props) => <DashboardScreen {...props} />}</Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
